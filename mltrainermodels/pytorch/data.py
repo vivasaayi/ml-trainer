d2c473c87bf8e466062241d6e563d63fd468a5a8
vivasaayi@live.com <vivasaayi@live.com>
2022-05-15 04:44:51 +0000

    Local Changes
    Local Changes

--- mltrainermodels/pytorch/data.py
+++ mltrainermodels/pytorch/data.py
@@ -1,12 +1,14 @@
 from torchvision import datasets, models,transforms as T
 from torch.utils.data import DataLoader
+import torch
 
-base_path = "/Users/rajanp/Downloads/rice/RiceDiseaseDataset"
+base_path = "/home/ec2-user/SageMaker/data/rice-diseases-image-dataset/RiceDiseaseDataset"
+num_workers=4
 
 transform = T.Compose(
     [
-        T.Resize(126),
-        T.CenterCrop(126),
+        T.Resize(64),
+        T.CenterCrop(64),
         T.ToTensor(),
         T.Normalize([0.485, 0.456, 0.406],
                              [0.229, 0.224, 0.225])
@@ -20,7 +22,7 @@ def get_training_data_loder(batch_size):
     #     download=True,
     #     transform=transform
     # )
-    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+    train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     print(train_dataloader)
     # ds
     return train_dataloader
@@ -33,5 +35,5 @@ def get_test_data_loader(batch_size):
     #     download=True,
     #     transform=transform
     # )
-    test_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     return test_dataloader
