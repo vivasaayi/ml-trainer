c59f8be48d307ebac88697a1b5d8bfb228b4957a
Rajan Panneer Selvam <vivasaayi@live.com>
2022-05-15 00:45:45 -0400

    Test Resnet

--- commands/rice.py
+++ commands/rice.py
@@ -2,7 +2,7 @@ import click
 
 from trainer.keras_trainer import KerasMLTrainer
 from trainer.pytorch_trainer import PyTorchMLTrainer
-from datasets.keras.mnist import MnistDataSet
+from datasets.keras.cifar10 import Cifar10DataSet
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
@@ -18,24 +18,15 @@ def train():
     pass
 
 @click.command()
-def keras_simple_convnet():
-    mnist_dataset = MnistDataSet()
-    ml_trainer = KerasMLTrainer("local.simple_convnet", mnist_dataset)
-    ml_trainer.initialize()
-    ml_trainer.train(128, 3)
-    ml_trainer.save_model()
-
-@click.command()
 def torch_simple_convnet():
-    #mnist_dataset = PyTorchMLTrainer()
-    training_data = datasets.MNIST(
+    training_data = datasets.CIFAR10(
         root="data",
         train=True,
         download=True,
         transform=ToTensor()
     )
 
-    test_data = datasets.MNIST(
+    test_data = datasets.CIFAR10(
         root="data",
         train=False,
         download=True,
@@ -48,7 +39,34 @@ def torch_simple_convnet():
     ml_trainer.save_model()
 
 
+@click.command()
+def torch_resnet18():
+    training_data = datasets.CIFAR10(
+        root="data",
+        train=True,
+        download=True,
+        transform=ToTensor()
+    )
+
+    test_data = datasets.CIFAR10(
+        root="data",
+        train=False,
+        download=True,
+        transform=ToTensor()
+    )
+
+    ml_trainer = PyTorchMLTrainer("torch_models.resnet18", training_data, test_data)
+    ml_trainer.initialize()
+    ml_trainer.train()
+    ml_trainer.save_model()
+
+
+train.add_command(keras_cnn)
 train.add_command(keras_simple_convnet)
+
+train.add_command(keras_ka_resnet50)
+
 train.add_command(torch_simple_convnet)
+train.add_command(torch_resnet18)
 
 rice.add_command(train)
